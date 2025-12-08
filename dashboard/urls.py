155f5a769856cb1963ee01b0356d24537cae77e1
smj3348 <61986133+smj3348@users.noreply.github.com>
2025-12-08 15:39:03 -0500

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path("", views.dashboard_home, name="dashboard"),
+
+    # New pages
+    path("monitoring/", views.monitoring_page, name="monitoring"),
+    path("provisioning/", views.provisioning_page, name="provisioning"),
+    path("jobs/", views.jobs_page, name="jobs"),
+    path("docs/", views.docs_page, name="docs"),
+    path("settings/", views.settings_page, name="settings"),
 ]
